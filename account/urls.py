eb99b3468d388909e8246337cd3bc7d778a1b9f2
loneranger <loneranger547@gmail.com>
2023-02-23 13:04:30 +0545

    added update token

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path,include
 from .views import *
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 urlpatterns=[
+    path('updatetoken/',TokenRefreshView.as_view(),name="refresh"),
     path('signup/',SignUpViews.as_view(),name="signup"),
     path('login/',LoginViews.as_view(),name="login"),
     path('checkbussinessname/',CheckBussinessName.as_view(),name="checkbussinessname"),
