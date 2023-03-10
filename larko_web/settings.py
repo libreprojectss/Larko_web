79ce31eaf5543eab79583b80cff3addc80e81f6f
loneranger <loneranger547@gmail.com>
2023-03-10 23:25:45 +0545

    some changes for deployment

--- larko_web/settings.py
+++ larko_web/settings.py
@@ -91,28 +91,28 @@ AUTH_USER_MODEL='account.User'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {  
-    'default': {  
-        'ENGINE': 'django.db.backends.postgresql',  
-        'NAME': 'larko_db',  
-        'USER': 'postgres',  
-        'PASSWORD': 'postgres',  
-        'HOST': 'localhost',  
-        'PORT': '5432',  
-        
-    }  
-}
 # DATABASES = {  
 #     'default': {  
 #         'ENGINE': 'django.db.backends.postgresql',  
-#         'NAME': 'railway',  
+#         'NAME': 'larko_db',  
 #         'USER': 'postgres',  
-#         'PASSWORD': '6QxWsGxabWkf1bdIwIXf',  
-#         'HOST': 'containers-us-west-36.railway.app',  
-#         'PORT': '5902',  
+#         'PASSWORD': 'postgres',  
+#         'HOST': 'localhost',  
+#         'PORT': '5432',  
         
 #     }  
 # }
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.postgresql',  
+        'NAME': 'railway',  
+        'USER': 'postgres',  
+        'PASSWORD': '6QxWsGxabWkf1bdIwIXf',  
+        'HOST': 'containers-us-west-36.railway.app',  
+        'PORT': '5902',  
+        
+    }  
+}
 
 
 # Password validation
