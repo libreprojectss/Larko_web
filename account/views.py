b2f76ca670d9e7c4e00a41d0a034fb24d94fd349
loneranger <loneranger547@gmail.com>
2023-03-12 19:06:48 +0545

    some changes

--- account/views.py
+++ account/views.py
@@ -62,7 +62,7 @@ class Business_Profile_Views(APIView):
     
     def put(self,request):
         objectvalue=Business_Profile.objects.get(user=request.user)
-        serializer=Business_Profile_Serializer(instance=objectvalue,partial=True)
+        serializer=Business_Profile_Serializer(instance=objectvalue,data=request.data,partial=True)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response({"msg":"Updated successfully","data":serialize.data},status=status.HTTP_200_OK)
