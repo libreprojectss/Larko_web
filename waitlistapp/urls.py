47bb69708882e490d7ab0ecd47e4446a746d19d2
loneranger455-maker <rangerlone547@gmail.com>
2023-05-01 20:29:37 +0545

    added serving and sserved feature and fixed resources issues

--- waitlistapp/urls.py
+++ waitlistapp/urls.py
@@ -10,11 +10,11 @@ urlpatterns=[
     path('notes/<str:cid>/<str:nid>/',NotesView.as_view(),name="notes"),
     path('services/',ServicesViews.as_view(),name="services"),
     path('services/<str:pk>/',ServicesViews.as_view(),name="services_edit_delete"),
-    path('serving/<str:pk>/',Servinglist.as_view(),name="serving_list"),
-    path('serving/',Servinglist.as_view(),name="serving"),
-    path('served/',Servedlist.as_view(),name="served"),
-    path('served/<str:pk>/',Servedlist.as_view(),name="served_history"),
-    path('resources/',ResourcesViews.as_view(),name="resources"),
-    path('resources/<str:pk>/',ResourcesViews.as_view(),name="resources")
+    path('serving/<str:pk>/',Servinglist.as_view(),name="serving_list"), #send waitlist id from waitlist to be served here in pk in post request
+    path('serving/',Servinglist.as_view(),name="serving"),    #for get request 
+    path('served/',Servedlist.as_view(),name="served"),        #send serving id from serving list to be served here in pk in post request
+    path('served/<str:pk>/',Servedlist.as_view(),name="served_history"),  #for get request 
+    path('resources/',ResourcesViews.as_view(),name="resources"),  #for resources get and post
+    path('resources/<str:pk>/',ResourcesViews.as_view(),name="resources")    #For resources put and delete. can send services along with the data
 
 ]
