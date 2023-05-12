e59c166afc5451dfa6b76a37375c7078b5ba64c4
loneranger455-maker <rangerlone547@gmail.com>
2023-05-12 15:42:57 +0545

    some changes

--- larko_web/settings.py
+++ larko_web/settings.py
@@ -188,7 +188,17 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 CORS_ORIGIN_ALLOW_ALL = True  
+CORS_ALLOW_HEADERS = [
+    "content-type",
+    "X-CSRFToken"
+]
+CORS_EXPOSE_HEADERS =[
+    "content-type",
+    "X-CSRFToken"
+]
 
+CSRF_COOKIE_SAMESITE='Lax'
+CSRF_COOKIE_HTTPONLY =True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:5173',
