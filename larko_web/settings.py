92a42118eca59612f67d72b11faf18758882e025
loneranger455-maker <rangerlone547@gmail.com>
2023-05-14 16:10:31 +0545

    added websockets for realtime data

--- larko_web/settings.py
+++ larko_web/settings.py
@@ -91,14 +91,19 @@ REST_FRAMEWORK = {
 
 }
 AUTH_USER_MODEL='account.User'
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels_redis.core.RedisChannelLayer",
+#         "CONFIG": {
+#             "hosts": [("localhost", 6379)],
+#         },
+#     },
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("localhost", 6379)],
-        },
-    },
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
 }
+# }
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
