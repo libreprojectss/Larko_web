aea784e88ac3fd7bfb71d7f5e652d387171503f4
Pujan Lamichhane <58980366+loneranger455-maker@users.noreply.github.com>
2023-05-22 12:25:05 +0545

    Update Status.jsx

--- src/Customer/components/Status.jsx
+++ src/Customer/components/Status.jsx
@@ -1,16 +1,24 @@
 import axios from 'axios'
 import Cookie from 'js-cookie';
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
-function Status({id}) {
+function Status({ id }) {
+    const navigate=useNavigate()
     const [msg, setMsg] = useState({});
     function removeFromQueue() {
-        const queue_cookie=Cookie.get("queue_cookies")
-        axios.post(`http://127.0.0.1:8000/api/publiclink/removedata/${id}/`,{"validation_token":queue_cookie}).then(res => {setMsg(res.message);console.log(res)}).catch((err) => console.log(err));
+        const queue_cookie = Cookie.get("queue_cookies")
+        axios.post(`http://127.0.0.1:8000/api/publiclink/removedata/${id}/`, { "validation_token": queue_cookie }).then(res => {
+            console.log('I left')
+            setMsg(res.message);
+            Cookie.remove('queue_cookies')
+            navigate(`/publicjoin/${id}/`);
+            console.log(res)
+        }).catch((err) => console.log(err));
     }
     return (
         <>
- 
+
             <div className='mt-6'>
                 <button
                     onClick={removeFromQueue}
